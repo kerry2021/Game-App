a9251ab52b44434b0aea8aaed04618b2faa48dec
865549906 <z865549906@gmail.com>
2019-10-29 03:27:23 -0400

    commit main activity for reaction game, then working on details

--- phase1/app/src/main/java/com/example/gameproject/reaction_game/reactionGameActivity.java
+++ phase1/app/src/main/java/com/example/gameproject/reaction_game/reactionGameActivity.java
@@ -1,6 +1,9 @@
 package com.example.gameproject.reaction_game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,24 @@ import com.example.gameproject.R;
 
 public class reactionGameActivity extends AppCompatActivity {
 
+    Button play;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reaction_game);
+        setContentView(R.layout.reaction_game_menu);
+        play = findViewById(R.id.startgame);
+    }
+
+    public void Listener(View v) {
+        if (v.getId()==R.id.startgame){
+            Play();
+        }
+    }
+
+
+    private void Play() {
+        Intent play = new Intent(this, reactionGame.class);
+        startActivity(play);
     }
 }
