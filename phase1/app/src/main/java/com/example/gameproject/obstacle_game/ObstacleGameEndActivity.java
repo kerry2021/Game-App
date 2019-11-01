0c0f3be3aeec8c397b02917bca74eebcf3bb3746
yhschan <yhschan1637@gamil.com>
2019-11-01 00:17:25 -0400

    Accomplish Customization Part.

--- phase1/app/src/main/java/com/example/gameproject/obstacle_game/ObstacleGameEndActivity.java
+++ phase1/app/src/main/java/com/example/gameproject/obstacle_game/ObstacleGameEndActivity.java
@@ -16,17 +16,17 @@ public class ObstacleGameEndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obstacle_game_end);
+        setTitle("Obstacle Game Result");
 
         Bundle bundle = getIntent().getExtras();
         String score = bundle.getString("score");
+        score = "Your Score is " + score;
 
         final TextView thanks = findViewById(R.id.thanks_for_playing_obstacle);
         final TextView score_text = findViewById(R.id.obstacle_game_score);
 
         thanks.setText(R.string.thanks_obstacle_game);
-        SpannableString content = new SpannableString(score);
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        score_text.setText(content);
+        score_text.setText(score);
     }
 
 }
