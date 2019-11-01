c08829bb37cdf95b909dee89f0d32b882cb4f736
liumin53 <gavin.liu@mail.utoronto.ca>
2019-11-01 19:32:43 -0400

    Added countdown time customization option.

--- phase1/app/src/main/java/com/example/gameproject/puzzle_game/PuzzleGameIntroActivity.java
+++ phase1/app/src/main/java/com/example/gameproject/puzzle_game/PuzzleGameIntroActivity.java
@@ -28,15 +28,21 @@ public class PuzzleGameIntroActivity extends AppCompatActivity {
     public final static String THREE_BY_THREE = "3 by 3 puzzle";
     public final static String FOUR_BY_FOUR = "4 by 4 puzzle";
     public final static String FIVE_BY_FIVE = "5 by 5 puzzle";
+    public final static String FOUR_MIN = "Easy: 4 minutes";
+    public final static String TWO_MIN = "Normal: 2 minutes";
+    public final static String ONE_MIN = "Hard 1 minutes";
 
     public static int customizedColumns = 3;//default value: 3 by 3 puzzle
-    private Spinner spinner;
+    public static int countDownTime = 120000;// default value 2 min
+    private Spinner dimSpinner, timeSpinner;
 
     /**
      * The list of choices for the spinner that allows the user to choose which
      * Java feature to demonstrate.
      */
     private final String[] puzzleDimensions = {THREE_BY_THREE, FOUR_BY_FOUR, FIVE_BY_FIVE};
+    private final String[] countDownTimeChoice = {TWO_MIN, FOUR_MIN, ONE_MIN};
+
     private String backgroundColour = "#FFFFFF";
 
     @Override
@@ -59,6 +65,7 @@ public class PuzzleGameIntroActivity extends AppCompatActivity {
         startPuzzleGameButton.setOnClickListener(v -> {
             Intent toGame = new Intent(v.getContext(), puzzleGameActivity.class);
             toGame.putExtra("background", backgroundColour);
+            toGame.putExtra("countDownTime", countDownTime);
             startActivity(toGame);
         });
 
@@ -98,12 +105,11 @@ public class PuzzleGameIntroActivity extends AppCompatActivity {
             addImageButton.setOnClickListener(view1 ->
                     startActivity(new Intent(view.getContext(), ImageActivity.class)));
 
-            changeBackgroundButton.setOnClickListener(v -> {
-                startActivity(new Intent(v.getContext(), BackgroundChangeActivity.class));
-            });
+            changeBackgroundButton.setOnClickListener(v ->
+                    startActivity(new Intent(v.getContext(), BackgroundChangeActivity.class)));
 
             confirmButton.setOnClickListener(view12 -> {
-                switch (spinner.getSelectedItem().toString()) {
+                switch (dimSpinner.getSelectedItem().toString()) {
                     case THREE_BY_THREE:
                         customizedColumns = 3;
                         break;
@@ -112,6 +118,18 @@ public class PuzzleGameIntroActivity extends AppCompatActivity {
                         break;
                     case FIVE_BY_FIVE:
                         customizedColumns = 5;
+                        break;
+                }
+
+                switch (timeSpinner.getSelectedItem().toString()) {
+                    case TWO_MIN:
+                        countDownTime = 120000;
+                        break;
+                    case FOUR_MIN:
+                        countDownTime = 240000;
+                        break;
+                    case ONE_MIN:
+                        countDownTime = 60000;
                         break;
                 }
                 popupWindow.dismiss();
@@ -129,11 +147,18 @@ public class PuzzleGameIntroActivity extends AppCompatActivity {
 
     private void setUpSpinner(View view) {
         // Set up the puzzleDimensions spinner.
-        spinner = (Spinner) view.findViewById(R.id.spinner);
-        ArrayAdapter adapter = new ArrayAdapter(
+        dimSpinner = (Spinner) view.findViewById(R.id.spinner1);
+        ArrayAdapter adapter1 = new ArrayAdapter(
                 this, android.R.layout.simple_spinner_item, puzzleDimensions);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dimSpinner.setAdapter(adapter1);
+
+        // Set up the countDownTime spinner
+        timeSpinner = view.findViewById(R.id.spinner2);
+        ArrayAdapter adapter2 = new ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, countDownTimeChoice);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(adapter2);
     }
 
 
