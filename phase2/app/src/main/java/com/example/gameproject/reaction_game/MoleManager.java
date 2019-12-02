ac098c59fc4501a21c178cf225bb9420beb30608
865549906 <z865549906@gmail.com>
2019-12-01 23:45:39 -0500

    adding comment

--- phase2/app/src/main/java/com/example/gameproject/reaction_game/MoleManager.java
+++ phase2/app/src/main/java/com/example/gameproject/reaction_game/MoleManager.java
@@ -10,13 +10,33 @@ import java.util.HashMap;
 import java.util.Observable;
 
 public class MoleManager extends Observable{
-
+    /**
+     * single mole object
+     */
     private Moles moles;
+    /**
+     * the positions that can be chosen to hit
+     */
     private ImageButton hitPosition;
+    /**
+     * the count down time
+     */
     private TimeThread time;
+    /**
+     * the count down time for mole
+     */
     private MoleThread generateMole;
+    /**
+     * behaviour when hit
+     */
     private ClickImage clicker;
+    /**
+     * whether or not we hit a mole
+     */
     private boolean hit;
+    /**
+     * how long has the game started
+     */
     private int timer, currStep,running;
 
     public MoleManager(ReactionGameActivity reaction,int speed){
@@ -34,7 +54,9 @@ public class MoleManager extends Observable{
         clicker.setReaction(this);
 
     }
-
+    /**
+     * update the next mole that is going to appeaar onto the screen
+     */
     public void updateScreen(int next, int step){
         running = 1;
         moles.generateNextMole(next);
@@ -42,7 +64,9 @@ public class MoleManager extends Observable{
         setChanged();
         notifyObservers();
     }
-
+    /**
+     * situation when we hit a mole
+     */
     public void ifHit (View v){
         running = 3;
         if (moles.checkSame(v)) {
@@ -65,11 +89,15 @@ public class MoleManager extends Observable{
         notifyObservers();
         timer--;
     }
-
+    /**
+     * return the duration since the game has started
+     */
     public int getTimer(){
         return this.timer;
     }
-
+    /**
+     * return the current score
+     */
     public int getScore(){
         return moles.getScore();
     }
@@ -77,43 +105,62 @@ public class MoleManager extends Observable{
     public int watRunning(){
         return running;
     }
-
+    /**
+     * the position that we hit
+     */
     public ImageButton getHitPosition(){
         return this.hitPosition;
     }
-
+    /**
+     * whether or not we hit a mole
+     */
     public boolean hitMole(){
         return this.hit;
     }
-
+    /**
+     * how many steps we have taken in total
+     */
     public int getStep(){
         return this.currStep;
     }
-
+    /**
+     * find the next mole that is going to appear on to the screen
+     */
     public ImageButton getNextMole(){
         return moles.getNextMole();
     }
-
+    /**
+     * the minimap for the moles
+     */
     public ImageButton[] getAllMoles(){
         return moles.getAllMoles();
     }
-
+    /**
+     * pause the game(time thread, mole thread)
+     */
     public void pause(){
         clicker.setMovable(false);
         generateMole.setRunning(false);
         time.setRunning(false);
     }
-
+    /**
+     * resume all timer that has been paused and start producing moles
+     */
     public void resume(){
         clicker.setMovable(true);
         generateMole.setRunning(true);
         time.setRunning(true);
     }
+    /**
+     * start the game
+     */
     public void start(){
         generateMole.start();
         time.start();
     }
-
+    /**
+     * stop current game
+     */
     public void stop(){
         generateMole.interrupt();
         time.interrupt();
